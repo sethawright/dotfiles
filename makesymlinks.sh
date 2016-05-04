b2c96a539211b5e41271f462ba338f5458b72b8c
Seth Wright <asethwright@gmail.com>
2016-05-04 09:06:15 -0400

    Remove bash stuff.

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -10,7 +10,7 @@ dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
 
 # list of files/folders to symlink in homedir
-files="bashrc bash_profile vim vimrc tmux.conf gvimrc gitconfig gitignore_global viminfo zshrc hammerspoon mutt muttrc"
+files="vim vimrc tmux.conf gvimrc gitconfig gitignore_global viminfo zshrc hammerspoon mutt muttrc"
 
 ##########
 
