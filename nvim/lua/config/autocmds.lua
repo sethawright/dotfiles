c1852f5ce76d3e64354de0e71b95233b8e6bfa6a
Seth Wright <seth.wright@willowtreeapps.com>
2024-02-12 19:40:31 -0500

    Markdown stuff

---
+++ nvim/lua/config/autocmds.lua
@@ -0,0 +1,16 @@
+local function augroup(name)
+  return vim.api.nvim_create_augroup("lazyvim_" .. name, { clear = true })
+end
+
+-- wrap and check for spell in text filetypes
+vim.api.nvim_create_autocmd("FileType", {
+  group = augroup("wrap_spell"),
+  pattern = { "gitcommit", "markdown" },
+  callback = function()
+    vim.opt_local.wrap = true
+    vim.opt_local.number = false
+    vim.opt_local.relativenumber = false
+    vim.opt_local.signcolumn = "no"
+    vim.opt_local.spell = true
+  end,
+})
