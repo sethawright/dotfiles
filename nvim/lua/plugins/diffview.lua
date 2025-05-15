694d2be50bdb2e2e058f16eee654873309d0a1ee
Seth Wright <103055742+sethawright@users.noreply.github.com>
2025-05-14 22:07:47 -0400

    Diffview fixes.

--- nvim/lua/plugins/diffview.lua
+++ nvim/lua/plugins/diffview.lua
@@ -3,19 +3,28 @@ return {
   lazy = true,
   keys = {
     { "<leader>gd", "<cmd>DiffviewOpen<cr>", desc = "Diffview Open" },
+    { "<leader>gm", "<cmd>DiffviewOpen main<cr>", desc = "Diffview Open" },
     { "<leader>gD", "<cmd>DiffviewClose<cr>", desc = "Diffview Close" },
     { "<leader>gh", "<cmd>DiffviewFileHistory<cr>", desc = "Diffview File History" },
     { "<leader>gH", "<cmd>DiffviewFileHistory %<cr>", desc = "Diffview Current File History" },
   },
   opts = {
+    show_help_hints = false,
     view = {
       merge_tool = {
         layout = "diff3_mixed",
       },
     },
+    file_panel = {
+      listing_style = "list",
+    },
     keymaps = {
+      file_panel = {
+        { "n", "q", "<Cmd>tabclose<CR>", { silent = true } },
+      },
       view = {
-        ["q"] = "<cmd>DiffviewClose<cr>",
+        { "n", "<leader>q", "<Cmd>tabclose<CR>", { silent = true } },
+        { "n", "<leader>q", "<Cmd>tabclose<CR>", { silent = true } },
       },
     },
   },
