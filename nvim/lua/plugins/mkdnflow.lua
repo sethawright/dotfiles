45a8d75e6a0983546a57cee69b1b79fd0dbc23ed
Seth Wright <103055742+sethawright@users.noreply.github.com>
2025-08-27 22:33:02 -0400

    Mkdnflow updates.

--- nvim/lua/plugins/mkdnflow.lua
+++ nvim/lua/plugins/mkdnflow.lua
@@ -5,6 +5,11 @@ return {
       require("mkdnflow").setup({
         filetypes = { md = true, rmd = true, markdown = true, text = true, txt = true },
         wrap = true,
+        mappings = {
+          MkdnCreateLinkFromClipboard = { { "n", "v" }, "<leader>l" },
+          MkdnFoldSection = false,
+          MkdnUnfoldSection = false,
+        },
       })
     end,
   },
